b380868f339406ec3395db5e19fb8be1708a54df
Lautaro Tresca <lautarotrescadesarrollador@gmail.com>
2023-09-28 19:26:25 -0300

    funcionalidades a los botones del nav

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const navNombre = document.getElementById("navNombre");
 const navDeveloper = document.getElementById("navDeveloper");
+const botonProyectos = document.getElementById("botonProyectos");
+const botonSobreMi = document.getElementById("botonSobreMi");
 
 setInterval(() => {
     navNombre.classList.toggle("subir");
     navDeveloper.classList.toggle("subir");
 }, 4000);
+
+botonSobreMi.addEventListener("click", () => {    
+    window.scrollTo(0, 990);
+});
+
+botonProyectos.addEventListener("click", () => {
+    window.scrollTo(0, 2000);
+});
